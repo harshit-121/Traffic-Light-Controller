bec0162db90745ea3c3fb5d46d8a9f36c20aa3b5
harshit-121 <164798984+harshit-121@users.noreply.github.com>
2024-07-18 13:11:12 +0530

    Update TLC.v

--- TLC.v
+++ TLC.v
@@ -1,22 +1,26 @@
 `timescale 1ns / 1ps
 
 module TLC(
-    input clk,reset,Xh,Xc,
-    output reg Gh,Yh,Rh,Gc,Yc,Rc
+    input clk,reset,Sa,Sb,
+    output reg Ga,Ya,Ra,Gb,Yb,Rb
     );
-    
-    
-    
+
     // G->Y->R->G 
+    // A is main street, higher priority than street B
     
-    parameter S0 = 3'b000;  // hgw=G, ctry=R
-    parameter S1 = 3'b001;  // hgw=Y, ctry=R
-    parameter S2 = 3'b010;  // hgw=R, ctry=R (Extra Safety)
-    parameter S3 = 3'b011;  // hgw=R, ctry=G
-    parameter S4 = 3'b100;  // hgw=R, ctry=Y
+    parameter S0 = 3'b000;  // A = G, B = R
+    parameter S1 = 3'b001;  // A = Y, B = R
+    parameter S2 = 3'b010;  // A = R, B = R (Extra Safety)
+    parameter S3 = 3'b011;  // A = R, B = G
+    parameter S4 = 3'b100;  // A = R, B = Y
     
     reg [2:0] pr_state, nxt_state;
     reg flag = 1'b0;
+    
+    initial begin
+    pr_state = 0;
+    nxt_state = 0;
+    end
     
     always@(posedge clk)begin
         if(reset)
@@ -27,68 +31,72 @@ module TLC(
     
     // Logic for next state
     
-    always@(pr_state,Xh,Xc)begin
+    always@(pr_state,Sa,Sb)begin
         case(pr_state)
             S0: begin 
-                if(flag == 0)
-                repeat(5)@(posedge clk);
-                flag =1;
-                if(Xc == 1'b1)begin
-                    
-                    nxt_state = S1;
-                    end
-                else
-                    nxt_state = S0;
+                    if(flag == 0)
+                        repeat(5)@(posedge clk);
+                    flag =1;
+                    if(Sb == 1'b1)
+                        nxt_state = S1;
+                    else
+                        nxt_state = S0;
                 end
             S1: begin
-                   // repeat(y2r_delay)@(posedge clk);
                     nxt_state = S2;
                     flag= 0;
                 end
             S2: begin
-                    //repeat(r2g_delay) @(posedge clk) ;
                     nxt_state = S3;
                 end
             S3: begin 
-            if(flag==1'b0)
-            repeat(4) @(posedge clk);
-            else
-            repeat(1) @(posedge clk);
-            flag = 1'b1;
-            if(Xc == 1'b1 && Xh == 1'b0)
-                    nxt_state = S3;
-                else 
-                    nxt_state = S4;
-             end
+                    if(flag==1'b0)
+                        repeat(4) @(posedge clk);
+                    else
+                        repeat(1) @(posedge clk);
+                    flag = 1'b1;
+                    if(Sb == 1'b1 && Sa == 1'b0)
+                        nxt_state = S3;
+                    else 
+                        nxt_state = S4;
+                end
             S4: begin
-                    //repeat(y2r_delay) @(posedge clk);
                     nxt_state = S0;
                     flag = 1'b0;
                 end
+        default: nxt_state = S0;
         endcase
     end
+    
+    // Logic for output
     
     always@(pr_state)begin
         case(pr_state)
             S0: begin 
-                    Rh=0;Yh=0;Gh=1;Rc=1;Yc=0;Gc=0;
+                    Ra=0;Ya=0;Ga=1;
+                    Rb=1;Yb=0;Gb=0;
                 end
             S1: begin 
-                    Rh=0;Yh=1;Gh=0;Rc=1;Yc=0;Gc=0;
+                    Ra=0;Ya=1;Ga=0;
+                    Rb=1;Yb=0;Gb=0;
                 end
             S2: begin 
-                    Rh=1;Yh=0;Gh=0;Rc=1;Yc=0;Gc=0;
+                    Ra=1;Ya=0;Ga=0;
+                    Rb=1;Yb=0;Gb=0;
                 end
             S3: begin 
-                    Rh=1;Yh=0;Gh=0;Rc=0;Yc=0;Gc=1;
+                    Ra=1;Ya=0;Ga=0;
+                    Rb=0;Yb=0;Gb=1;
                 end
             S4: begin 
-                    Rh=1;Yh=0;Gh=0;Rc=0;Yc=1;Gc=0;
+                    Ra=1;Ya=0;Ga=0;
+                    Rb=0;Yb=1;Gb=0;
+                end
+       default: begin 
+                    Ra=0;Ya=0;Ga=1;
+                    Rb=1;Yb=1;Gb=0;
                 end
         endcase
     end
     
-    
 endmodule
-
-
